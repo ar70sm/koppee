8554c9dfae8530e3b566561e649ee0de88eff364
Abdallah Ashraf <ar70sm@gmail.com>
2023-03-22 16:45:22 +0200

    finishing the pages

--- js/script.js
+++ js/script.js
@@ -1,31 +1,51 @@
-// very simple carousel on the wrapper
+document.querySelector("nav > i.list").onclick=()=>document.querySelector("nav > i.list").classList.toggle("active")
 
-let wrapCarousel =document.querySelectorAll("#wrapper ul li");
-let wrapCarouselButton =document.querySelectorAll("#wrapper > i");
+// carousel of wrapper
 
-
-let wrapInterval=setInterval(()=>{
-  wrapCarousel.forEach(e=>{
-    e.classList.toggle("active")
-  })
-},5000)
-wrapCarouselButton.forEach(e=>{
-  e.onclick=()=>{
-    wrapCarousel.forEach(e=>{
-      e.classList.toggle("active")
-    })  
-    clearInterval(wrapInterval)
-    wrapInterval=setInterval(()=>{
-      wrapCarousel.forEach(e=>{
-        e.classList.toggle("active")
-      })
-    },5000)
-  }
-})
-
+$("#wrapper .owl-carousel").owlCarousel({
+  loop: true,
+  nav: true,
+  autoplay: true,
+  mouseDrag: false,
+  dots: false,
+  smartSpeed: 1000,
+  items: 1,
+});
 // go to up button
 
-addEventListener("scroll",()=>{
-  if (window.scrollY>50) document.querySelector(".goUp").classList.add("seen")
-  else document.querySelector(".goUp").classList.remove("seen")
-})
+addEventListener("scroll", () => {
+  if (window.scrollY > 50)
+    document.querySelector(".goUp").classList.add("seen");
+  else document.querySelector(".goUp").classList.remove("seen");
+});
+
+// carousel of testimonial
+
+$("#testimonial .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  margin:20,
+  mouseDrag: false,
+  smartSpeed: 700,
+  autoplayTimeout:10000,
+  center:true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+let mapCounter=0
+let mabBox = document.querySelector("#contact .map")
+setInterval(()=>{
+  mabBox.style.backgroundImage=`linear-gradient(45deg ,transparent ${mapCounter-20}%,#fffa ${mapCounter-20}%,#fffa ${mapCounter}%, transparent ${mapCounter}%)`
+  mapCounter++
+  mapCounter>400 ?mapCounter=0:mapCounter++
+},8)
